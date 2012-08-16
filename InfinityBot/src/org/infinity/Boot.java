240ca8bbceceb3b147ee3b48f3b52fd840df89a8
TheLimitInfinity <thelimitinfintity@hotmail.com>
2012-08-15 21:54:52 -0400

    Threaded downloads to speed up loading time

--- InfinityBot/src/org/infinity/Boot.java
+++ InfinityBot/src/org/infinity/Boot.java
@@ -1,13 +1,16 @@
 package org.infinity;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 public class Boot {
-	public static void main(final String args[]) throws Exception {
+	public static void main(final String args[]) throws Exception {	
 		try {
 			String location = Boot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 			location = location.replace('\\', '/');
@@ -21,18 +24,33 @@ public class Boot {
 			builder.append(" -Xmx1024m");
 			builder.append(" -Xbootclasspath/p:\"");
 			final File baseFolder = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "InfinityBot" + File.separator + "Lib");
-            final File [] jarLoc = {new File(baseFolder + File.separator + "InfinityX.jar"),new File(baseFolder + File.separator + "substance.jar"),new File(baseFolder + File.separator + "trident.jar"),new File(baseFolder + File.separator + "Icons.jar")};
+			final File [] jarLoc = {new File(baseFolder + File.separator + "InfinityX.jar"),new File(baseFolder + File.separator + "substance.jar"),new File(baseFolder + File.separator + "trident.jar"),new File(baseFolder + File.separator + "Icons.jar")};
 			for(int i = 0; i < jarLoc.length; i++){
 				builder.append(jarLoc[i]+";");
 				if(!jarLoc[i].exists()){
-				    new File(jarLoc[i].getParent()).mkdir();
-					jarLoc[i].createNewFile();
-					final URL website = new URL("http://www.infinitybot.org/bot/" + jarLoc[i].getName());
-				    final ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-				    final FileOutputStream fos = new FileOutputStream(jarLoc[i]);
-				    fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-				    rbc.close();
-				    fos.close();
+					final File curFile = jarLoc[i];
+					new File(curFile.getParent()).mkdir();
+					curFile.createNewFile();
+					new Thread(){
+						@Override
+						public void run() {
+							try {
+								URL website = new URL("http://www.infinitybot.org/bot/" + curFile.getName());
+								final ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+								final FileOutputStream fos = new FileOutputStream(curFile);
+								fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+								rbc.close();
+								fos.close();
+							} catch (MalformedURLException e) {
+								e.printStackTrace();
+							} catch (FileNotFoundException e) {
+								e.printStackTrace();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}			
+						}
+
+					}.start();
 				}
 			}
 			builder.append("\" org.infinity.ui.InfinityGUI");
